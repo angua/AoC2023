04923e9669a0b3544d21c66823fa7a727519ac39
Angua <birgit@angua.at>
2023-09-11 11:02:54 +0200

    find days in assembly using reflection

--- AoC2023/MainViewModel.cs
+++ AoC2023/MainViewModel.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.ObjectiveC;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
@@ -20,6 +22,24 @@ namespace AoC2023
             set => SetValue(value);
         }
 
+        public MainViewModel()
+        {
+            // find days in assembly using reflection
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            // find all classes
+            foreach (var type in executingAssembly.GetTypes().Where(t => t.IsClass))
+            {
+                // each day has a day provider interface 
+                if (type.FindInterfaces((type, criteria) => type == typeof(IDayProvider), null).Length > 0)
+                {
+                    var provider = (IDayProvider)Activator.CreateInstance(type);
+                    Days.Add(provider.GetDay());
+                }
+            }
 
+            Days = Days.OrderBy(d => d.DayNumber).ToList();
+        }
+        
+    
     }
 }

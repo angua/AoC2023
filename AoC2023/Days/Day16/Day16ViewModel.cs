cfff614cbc52a84c01f70bb1012ece01d41d2552
Angua <birgit@angua.at>
2023-12-16 07:43:14 +0100

    Day 16 - laser beams - input

---
+++ AoC2023/Days/Day16/Day16ViewModel.cs
@@ -0,0 +1,9 @@
+namespace AoC2023.Days.Day16;
+
+public class Day16ViewModel
+{
+        public Day16ViewModel()
+        {
+            var fileData = ResourceUtils.LoadDataFromResource("Day16", "input.txt");
+        }
+}

9c5dc66f5037d0accc5e868ab18785f0289941aa
Angua <birgit@angua.at>
2023-12-28 09:25:36 +0100

    Day 15 - start files

---
+++ AoC2023/Days/Day15/Day15ViewModel.cs
@@ -0,0 +1,11 @@
+using Advent2023.Utils;
+
+namespace AoC2023.Days.Day15;
+
+public class Day15ViewModel
+{
+        public Day15ViewModel()
+        {
+            var fileData = ResourceUtils.LoadDataFromResource("Day15", "input.txt");
+        }
+}

9de57bb4f1df25e02ab8d45fde84c1b5edb22252
Angua <birgit@angua.at>
2023-12-30 05:19:35 +0100

    Day 21 - part 2

--- AoC2023/Days/Day21/Day21ViewModel.cs
+++ AoC2023/Days/Day21/Day21ViewModel.cs
@@ -8,11 +8,13 @@ public class Day21ViewModel
     public WalkingElf Walker { get; set; } = new();
 
     public long Plots64Steps { get; set; }
+    public long Plots26MSteps { get; set; }
     public Day21ViewModel()
     {
         var fileData = ResourceUtils.LoadDataFromResource("Day21", "input.txt");
         Walker.Parse(fileData);
 
         Plots64Steps = Walker.GetPlots64Steps();
+        Plots26MSteps = Walker.GetPlots26MSteps();
     }
 }
--- AoC2023Lib/Days/Day21Lib/WalkingElf.cs
+++ AoC2023Lib/Days/Day21Lib/WalkingElf.cs
@@ -12,6 +12,9 @@ public class WalkingElf
 {
     private Dictionary<Vector2, char> _grid = new();
 
+    private int _countX;
+    private int _countY;
+
     public void Parse(Filedata fileData)
     {
         for (int y = 0; y < fileData.Lines.Count; y++)
@@ -24,14 +27,77 @@ public class WalkingElf
                 _grid[pos] = ground;
             }
         }
+        _countX = (int)_grid.Max(p => p.Key.X) + 1;
+        _countY = (int)_grid.Max(p => p.Key.Y) + 1;
     }
 
     public long GetPlots64Steps()
     {
-        return GetPlotsForSteps(64);
+        return GetPlotCountForSteps(64);
     }
 
-    private long GetPlotsForSteps(int numSteps)
+    public long GetPlots26MSteps()
+    {
+        var countAtFullGridWalk = new Dictionary<long, long>();
+
+        var start = _grid.First(p => p.Value == 'S').Key;
+        var positions = new HashSet<Vector2>();
+        positions.Add(start);
+
+        var steps = 0;
+
+        for (int i = 1; i <= 65; i++)
+        {
+            steps++;
+            var nextPositions = WalkToNewPositions(positions);
+            positions = nextPositions;
+        }
+        countAtFullGridWalk[steps] = positions.Count;
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int s  = 0; s < _countX; s++)
+            {
+                steps++;
+                var nextPositions = WalkToNewPositions(positions);
+                positions = nextPositions;
+            }
+            countAtFullGridWalk[steps] = positions.Count;
+        }
+
+        // solve quadratic equation
+        // y = a x^2 + bx + c
+
+        // a = ( (y3 - y2)(x3 - x1) - (y3 - y1)(x3 - x2) ) / ( (x3 ^2 - x2 ^2)(x3 - x1) - (x3 ^2 - x1 ^2)(x3 - x2) )
+        // b = ((y3 - y2) - a (x3 ^2 - x2 ^2)) / (x3 - x2)
+        // c = y3 - a * x3 ^ 2 - b x3
+
+        var x1 = (double)countAtFullGridWalk.Skip(1).First().Key;
+        var y1 = (double)countAtFullGridWalk.Skip(1).First().Value;
+
+        var x2 = (double)countAtFullGridWalk.Skip(2).First().Key;
+        var y2 = (double)countAtFullGridWalk.Skip(2).First().Value;
+                 
+        var x3 = (double)countAtFullGridWalk.Skip(3).First().Key;
+        var y3 = (double)countAtFullGridWalk.Skip(3).First().Value;
+
+        var a = ((y3 - y2) * (x3 - x1) - (y3 - y1) * (x3 - x2)) /
+                    ((Math.Pow(x3, 2.0) - Math.Pow(x2, 2.0)) * (x3 - x1) - (Math.Pow(x3, 2.0) - Math.Pow(x1, 2.0)) * (x3 - x2));
+
+        var b = ((y3 - y2) - a * (Math.Pow(x3, 2.0) - Math.Pow(x2, 2.0) )) / (x3 - x2);
+
+        var c = y3 - a * Math.Pow(x3, 2.0) - b * x3;
+
+        var targetSteps = 26501365;
+
+        var possibilities = a * Math.Pow(targetSteps, 2.0) + b * targetSteps + c;
+
+        return (long)possibilities;
+    }
+
+
+
+    private long GetPlotCountForSteps(int numSteps)
     {
         var start = _grid.First(p => p.Value == 'S').Key;
 
@@ -46,6 +112,10 @@ public class WalkingElf
         return positions.Count;
     }
 
+
+
+
+
     private HashSet<Vector2> WalkToNewPositions(HashSet<Vector2> positions)
     {
         var newPositions = new HashSet<Vector2>();
@@ -55,14 +125,30 @@ public class WalkingElf
             foreach (var dir in MathUtils.OrthogonalDirections)
             {
                 var newPos = pos + dir;
-                if (_grid[newPos] != '#')
+                if (GetGround(newPos) != '#')
                 {
                     // no stone, can walk here
                     newPositions.Add(newPos);
                 }
             }
         }
-
         return newPositions;
     }
+
+
+
+
+
+    private char GetGround(Vector2 pos)
+    {
+        var testPos = new Vector2(mod((int)pos.X, _countX), mod((int)pos.Y, _countY));
+        return _grid[testPos];
+    }
+
+    int mod(int x, int m)
+    {
+        int r = x % m;
+        return r < 0 ? r + m : r;
+    }
+
 }
